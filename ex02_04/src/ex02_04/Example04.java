531d2b384f650ff85962f92750df19d723a40026
jeon9825 <jeon9825@naver.com>
2018-10-07 00:24:58 +0900

    실습과제2_4) 메모리 구조

---
+++ ex02_04/src/ex02_04/Example04.java
@@ -0,0 +1,24 @@
+package ex02_04;
+
+class Person {
+	String name;
+	int age;
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
+
+public class Example04 {
+	public static void main(String[] args) {
+		Object[] a = new Object[3];
+		Object[] b = a;
+		
+		a[0] = new double[] { 1.1, 2.2, 3.3 };
+		a[1] = new String[] { "a", "b", "c" };
+		
+		Person p = new Person("¿”≤©¡§", 22);
+		a[2] = new Person[] { p, p };
+	}
+}
