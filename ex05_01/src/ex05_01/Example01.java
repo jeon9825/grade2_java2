a9845c1c5ba262c9d1ef46f75666497de0c49a30
jeon9825 <jeon9825@naver.com>
2018-10-28 14:32:49 +0900

    실습과제5_1)compareTo(), String.valueOf(int value)

---
+++ ex05_01/src/ex05_01/Example01.java
@@ -0,0 +1,29 @@
+package ex05_01;
+
+import java.util.Arrays;
+
+class Data3 implements Comparable<Data3> {
+	int a;
+
+	public Data3(int a) {
+		this.a = a;
+	}
+
+	@Override
+	public int compareTo(Data3 data) {
+		return this.a - data.a;
+	}
+
+	@Override
+	public String toString() {
+		return String.valueOf(a);
+	}
+}
+
+public class Example01 {
+	public static void main(String[] args) {
+		Data3[] a = new Data3[] { new Data3(3), new Data3(5), new Data3(1) };
+		Arrays.sort(a);
+		System.out.println(Arrays.toString(a));
+	}
+}
